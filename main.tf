3e0104b5f2430b6c6379f804c5a043d0c92198ab
Ben Mohamoodally <bmohamoodally@bmj.com>
2021-11-02 15:10:23 +0000

    updating ami

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ provider "aws" {
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
+  ami                    = "ami-047bb4163c506cd98"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
