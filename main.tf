0917694b88ccfc2cb6b6b5ec2302c217b31ee47b
Ben Mohamoodally <bmohamoodally@bmj.com>
2021-11-02 15:47:17 +0000

    updating ami

--- main.tf
+++ main.tf
@@ -22,15 +22,32 @@ terraform {
 
 
 provider "aws" {
-  region = "us-west-2"
+  region = "eu-west-1"
 }
 
-
+#vpc-0dfe34f926687e916
 
 resource "random_pet" "sg" {}
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
+#  ami                    = "ami-047bb4163c506cd98"
+  ami           = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
