27d592050c6602f0a08c6acab19d3905927836a4
Ben Mohamoodally <bmohamoodally@bmj.com>
2021-11-02 15:35:20 +0000

    updating ami

--- main.tf
+++ main.tf
@@ -22,14 +22,15 @@ terraform {
 
 
 provider "aws" {
-  region = "eu-west-1"
+  region = "us-west-2"
 }
+
 
 
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-047bb4163c506cd98"
+  ami                    = "ami-830c94e3"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
